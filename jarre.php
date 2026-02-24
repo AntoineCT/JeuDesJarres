02ca8acc721e5ab54c83394552aa0f4064299e07
Toinoufu <antoine.cabrera.turzo@gmail.com>
2026-02-24 15:39:58 +0100

    Niveau 3 fini

--- jarre.php
+++ jarre.php
@@ -9,23 +9,56 @@ $jarres = [
 ];
 
 $clePlace = 0;
-
-while($clePlace < 4) {
-    $cle = array_keys($jarres);
-    $cleAleatoire = $cle[rand(0, 4)];
-    if($jarres[$cleAleatoire] == "") {
-        $jarres[$cleAleatoire] = "clé";
-        $clePlace++;
-    }
-}
-
-foreach($jarres as $cle => $jarre) {
-    if($jarre == "") {
-        $jarres[$cle] = "serpent";
-    }
-}
-
 $cleTrouve = 0;
+
+$difficulte = readline("Choisissez une difficulté (1, 2, 3) : ");
+if($difficulte == 1) {
+    while($clePlace < 4) {
+        $cle = array_keys($jarres);
+        $cleAleatoire = $cle[rand(0, 4)];
+        if($jarres[$cleAleatoire] == "") {
+            $jarres[$cleAleatoire] = "clé";
+            $clePlace++;
+        }
+    }
+    foreach($jarres as $cle => $jarre) {
+        if($jarre == "") {
+            $jarres[$cle] = "serpent";
+        }
+    }
+}
+
+if($difficulte == 2) {
+    while($clePlace < 3) {
+        $cle = array_keys($jarres);
+        $cleAleatoire = $cle[rand(0, 4)];
+        if($jarres[$cleAleatoire] == "") {
+            $jarres[$cleAleatoire] = "clé";
+            $clePlace++;
+        }
+    }
+    foreach($jarres as $cle => $jarre) {
+        if($jarre == "") {
+            $jarres[$cle] = "serpent";
+        }
+    }
+}
+
+if($difficulte == 3) {
+    while($clePlace < 2) {
+        $cle = array_keys($jarres);
+        $cleAleatoire = $cle[rand(0, 4)];
+        if($jarres[$cleAleatoire] == "") {
+            $jarres[$cleAleatoire] = "clé";
+            $clePlace++;
+        }
+    }
+    foreach($jarres as $cle => $jarre) {
+        if($jarre == "") {
+            $jarres[$cle] = "serpent";
+        }
+    }
+}
 
 while($cleTrouve < 3) {
     $choix = readline("Saisissez un numéro de jarre entre 1 et 5 : ");
